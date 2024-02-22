9c34075faf2e3355494e13417e47cb4c0da9e3a7
“ishamanore” <ishamanore@gmail.com>
2024-02-22 15:58:00 +0530

    core policy

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("MyCookieAuthenticationScheme")
+    .AddCookie("MyCookieAuthenticationScheme", option =>
+    {
+        option.LogoutPath = "/User/Login";
+        option.AccessDeniedPath = "/User/AccessDenied";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(30); // Set your desired expiration time
+    });
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(5); // Set your desired timeout for session inactivity
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -15,6 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("corsapp");
+app.UseSession();
 
 app.UseHttpsRedirection();
 
