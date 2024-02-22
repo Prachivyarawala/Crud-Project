c98545742d6cbb8e4fefc6cd30c7566726d60681
Prachivyarawala <vyarawalaprachi1@gmail.com>
2024-02-22 17:07:43 +0530

    city views

---
+++ MVC/Controllers/CityController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MVC.Controllers
+{
+    [Route("[controller]")]
+    public class CityController : Controller
+    {
+        private readonly ILogger<CityController> _logger;
+
+        public CityController(ILogger<CityController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
