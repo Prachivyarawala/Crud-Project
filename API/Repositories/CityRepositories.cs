0d76932d9c3f34c1915ce4fc8fedfd2091d9fd16
“bandhankalkani” <bkalkani710@rku.ac.in>
2024-02-22 16:21:12 +0530

    changes in repo

--- API/Repositories/CityRepositories.cs
+++ API/Repositories/CityRepositories.cs
@@ -1,32 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Repositories
 {
-    [Route("[controller]")]
-    public class CityRepositories : Controller
+    public class CityRepositories
     {
-        private readonly ILogger<CityRepositories> _logger;
-
-        public CityRepositories(ILogger<CityRepositories> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+        
     }
 }
